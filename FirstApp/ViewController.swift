56db1f2eb203dcbfc41d1815cc06c969c7438af0
MindHack <MindHack@preemplionsmbp5.home>
2019-05-09 19:24:43 +0300

    primul meu comit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by MindHack on 07/05/2019.
+//  Copyright © 2019 MindHack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        myimage.isHidden = true
+        
+        mybutton.setTitle("arata imagine", for: .normal)
+        
+      
+    }
+    
+    
+    
+
+
+    @IBOutlet weak var myimage: UIImageView!
+    
+    
+    
+    
+    @IBOutlet weak var mybutton: UIButton!
+    
+    @IBAction func showHideImage(_ sender: Any) {
+       
+//        myimage.alpha = 0.5
+        myimage.isHidden = !myimage.isHidden
+        if myimage.isHidden {
+            mybutton.setTitle("arata imaginea", for: .normal)
+            mybutton.setTitleColor(.blue, for: .normal)
+            
+            
+        } else {
+            
+            mybutton.setTitle("ascunde imaginea", for: .normal)
+        }
+        
+
+    
+        
+        
+        
+        
+        
+    }
+    
+    
+}
+
